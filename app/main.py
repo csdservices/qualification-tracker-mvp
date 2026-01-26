3e14b89f6c31274841d4127908c93251b2afc168
Cal Dip <csdhello@gmail.com>
2026-01-26 11:21:56 +1100

    Switch app to FastAPI

--- app/main.py
+++ app/main.py
@@ -1,38 +1,15 @@
-from fastapi import FastAPI, Depends, Request, Form
-from fastapi.responses import HTMLResponse, RedirectResponse
+from fastapi import FastAPI, Request
+from fastapi.responses import HTMLResponse
 from fastapi.templating import Jinja2Templates
-from sqlalchemy.orm import Session
-
-from database import SessionLocal, engine
-from models import Base, Staff
 
 app = FastAPI()
+
 templates = Jinja2Templates(directory="app/templates")
 
-Base.metadata.create_all(bind=engine)
-
-def get_db():
-    db = SessionLocal()
-    try:
-        yield db
-    finally:
-        db.close()
 
 @app.get("/", response_class=HTMLResponse)
-def home(request: Request, db: Session = Depends(get_db)):
-    staff = db.query(Staff).all()
+def read_root(request: Request):
     return templates.TemplateResponse(
         "index.html",
-        {"request": request, "staff": staff}
+        {"request": request}
     )
-
-@app.post("/add")
-def add_staff(
-    first_name: str = Form(...),
-    last_name: str = Form(...),
-    db: Session = Depends(get_db)
-):
-    new_staff = Staff(first_name=first_name, last_name=last_name)
-    db.add(new_staff)
-    db.commit()
-    return RedirectResponse("/", status_code=303)
