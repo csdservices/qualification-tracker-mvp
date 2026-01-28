4fbae792dbb753125b749f4a5d7e21ec283b61b4
Cal Dip <csdhello@gmail.com>
2026-01-28 18:34:44 +1100

    Clean main app with DB test endpoint

--- app/main.py
+++ app/main.py
@@ -1,12 +1,29 @@
-from fastapi import FastAPI, Request
+from fastapi import FastAPI, Request, Depends
 from fastapi.responses import HTMLResponse
 from fastapi.templating import Jinja2Templates
+from sqlalchemy.orm import Session
+
+from .database import SessionLocal
+from . import models
 
 app = FastAPI()
 
+# Templates
 templates = Jinja2Templates(directory="app/templates")
 
 
+# ---------- Database Dependency ----------
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+
+# ---------- Routes ----------
+
+# Homepage
 @app.get("/", response_class=HTMLResponse)
 def read_root(request: Request):
     return templates.TemplateResponse(
@@ -15,20 +32,11 @@ def read_root(request: Request):
     )
 
 
-from fastapi import Depends
-from sqlalchemy.orm import Session
-from .database import SessionLocal
-from . import models
-
-# Dependency
-def get_db():
-    db = SessionLocal()
-    try:
-        yield db
-    finally:
-        db.close()
-
+# Test DB connection
 @app.get("/organisations")
 def read_organisations(db: Session = Depends(get_db)):
-    orgs = db.query(models.Organisation).all()
-    return [{"id": o.id, "name": o.name} for o in orgs]
+    organisations = db.query(models.Organisation).all()
+    return [
+        {"id": org.id, "name": org.name}
+        for org in organisations
+    ]
